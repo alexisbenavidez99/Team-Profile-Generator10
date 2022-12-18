a43ad93d43f92929ba91aa5d03e28b99dc6e71ba
alexisbenavidez99 <callmealexis49@gmail.com>
2022-12-18 17:00:23 -0600

    added template literals to append to html page

---
+++ src/page-template.js
@@ -0,0 +1,90 @@
+const generateTeam = team => {
+
+    const generateManager = manager => {
+        return `<div class="card" style="width: 20rem;">
+        <div class="card-body">
+          <h5 class="card-title">${manager.getName()}</h5>
+          <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h6>
+            <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">${manager.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></a></li>
+              <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+          </div>
+        </div>
+      </div>`
+    };
+
+    const generateEngineer = engineer => {
+        return `<div class="card" style="width: 20rem;">
+        <div class="card-body">
+          <h5 class="card-title">${engineer.getName()}</h5>
+          <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h6>
+          <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">ID: ${engineer.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+              <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>`
+    };
+
+    const generateIntern = intern => {
+        return `<div class="card" style="width: 20rem;">
+        <div class="card-body">
+          <h5 class="card-title">${intern.getName()}</h5>
+          <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h6>
+          <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">ID: ${intern.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+              <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+          </div>
+        </div>
+      </div>`
+    };
+
+    const htmlPage = [];
+
+    htmlPage.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    htmlPage.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    htmlPage.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return htmlPage.join("");
+}
+
+module.exports = team => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <h1>My Team</h1>
+        </div>
+        ${generateTeam}
+    </body>
+    </html>`
+}
